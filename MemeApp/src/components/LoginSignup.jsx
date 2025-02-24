91e49c8eef8876c63bf488f186dbf157800c2cad
jo-27 <joansharon270704@gmail.com>
2025-02-25 03:19:49 +0530

    routes created

---
+++ MemeApp/src/components/LoginSignup.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import user from "/login/user-icon.png";
+import password from "/login/password-icon.png";
+import email from "/login/email-icon.webp";
+import "./css/LoginSignup.css"
+const LoginSignUp = () => {
+  const [button,setButton]=useState("Sign up");
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{button}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {button==="Login"?<div></div>:<div className="input">
+            <img style={{width:"20px",height:"20px"}} src={user} alt="" />
+            <input type="text" placeholder="Enter Your Name"/>
+        </div>}
+        
+        <div className="input">
+            <img style={{width:"20px",height:"30px"}} src={email} alt="" />
+            <input type="email" placeholder="Enter your MailID"/>
+        </div>
+        <div className="input">
+            <img style={{width:"20px",height:"20px"}} src={password} alt="" />
+            <input type="password" placeholder="Enter the Password"/>
+        </div>
+      </div>
+      <div className="submit-container">
+        <div className={button==="Login"?"submit gray":"submit"} onClick={()=>setButton("Sign up")}>Sign up</div>
+        <div className={button==="Sign up"?"submit gray":"submit"} onClick={()=>setButton("Login")}>Login</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignUp;
---
+++ MemeApp/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./css/Navbar.css"
+const Navbar=()=> {
+  return (
+    <div className="navbar">
+      <img
+        src="/icons/main.png"
+        style={{ marginTop: "10px", width: "70px", heigth: "60px" }}
+        alt="icon"
+      />
+      <ul className="nav-menu">
+        <li>
+          <Link to="/home" className="link">Home</Link>
+        </li>
+        <li>
+          <Link to="/create" className="link">Create Meme</Link>
+        </li>
+        <li>
+          <Link to="/templates" className="link">Templates</Link>
+        </li>
+        <li >
+          <Link to="/saved" className="link">My Memes</Link>
+        </li>
+        
+      </ul>
+      <div ><Link to="/" className="nav-login" >login</Link></div>
+    </div>
+  )
+}
+
+export default Navbar
