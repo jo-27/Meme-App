eda0e82403ffbe3e31e0cf79524f84980a22f142
jo-27 <joansharon270704@gmail.com>
2025-02-27 01:55:07 +0530

    changed home

--- MemeApp/src/components/Home.jsx
+++ MemeApp/src/components/Home.jsx
@@ -12,14 +12,7 @@ const Home = () => {
         <h1>Welcome to MemeMaster</h1>
         <p>Create, Customize & Share Memes in Seconds!</p>
         <Link to="/Create" className="btn">Start Creating</Link>
-        <div className="features">
-            <ul>
-                <li>📸 Upload your images, Add text, stickers</li>
-                <li>🎭 Browse trending meme templates</li>
-                <li>🚀 Share instantly on social media</li>
-                <li>💾 Download high-quality memes</li>
-            </ul>
-        </div>
+        
     </div>
   )
 }
