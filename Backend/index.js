d96a02f4d6476ad213d44a868ac5bdb1c060bacf
jo-27 <joansharon270704@gmail.com>
2025-02-28 11:55:15 +0530

    signup backend implemented

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mdb = require("mongoose");
-const dotenv=require('dotenv')
+const dotenv=require('dotenv');
+const Signup=require("./models/signupSchema");
 const app = express();
 const PORT = 3001;
-dotenv.config()
+dotenv.config();
+app.use(express.json())
 mdb
   .connect("mongodb+srv://joansharon:sharon%4004@merndemo.bajzf.mongodb.net/Meme-App")
   .then(() => {
@@ -16,5 +18,23 @@ mdb
 app.get("/", (req, res) => {
   res.send("<h1>welcome back<h1>");
 });
+
+app.post("/signup",async(req,res)=>{
+    try {
+        const {name,email,password}=req.body
+    const newSignup=new Signup({
+        name:name,
+        email:email,
+        password:password
+    });
+    await newSignup.save()
+    console.log("signup sucess")
+    res.status(201).json({message:"Signup Successfull",isSignup:true})
+        
+    } catch (error) {
+        res.status(201).json({message:"Signup UnSuccessfull",isSignup:false})
+    }
+
+})
 
 app.listen(PORT, () => console.log("server started successfully"));
---
+++ Backend/models/signupSchema.js
@@ -0,0 +1,10 @@
+const mdb=require('mongoose')
+
+const signupSchema=mdb.Schema({
+    name:String,
+    email:String,
+    password:String,
+})
+
+const signup_schema=mdb.model("signup",signupSchema)
+module.exports=signup_schema
